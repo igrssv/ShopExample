d6d6c971f4252e16caef96055dd6bc1d7407df6e
igor <igro.sia@yandex.ru>
2022-07-26 15:57:17 +0300

    Initial Commit

---
+++ ShopExample/ShopExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopExampleApp.swift
+//  ShopExample
+//
+//  Created by Игорь Сысоев on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
